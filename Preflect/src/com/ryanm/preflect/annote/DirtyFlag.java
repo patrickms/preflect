d556b7ba726e43f99714560263c9393bf662ad2e
therealryan <therealryan@gmail.com>
2011-02-10 18:33:34 +0000

    Javadoc update

--- Preflect/src/com/ryanm/preflect/annote/DirtyFlag.java
+++ Preflect/src/com/ryanm/preflect/annote/DirtyFlag.java
@@ -10,11 +10,10 @@ import java.lang.annotation.Target;
 
 /**
  * Annotation for marking a boolean field or a void-return/no-arg
- * method. No widget will be generated for this field or method. The
- * boolean will be set to true, or the method called, if the value of
- * any other variable in the type is altered in a configuration
- * application. This provides a convenient way to react to
- * configuration changes without having to encapsulate every field
+ * method. The boolean will be set to true, or the method called, if
+ * the value of any other variable in the type is altered in a
+ * configuration application. This provides a convenient way to react
+ * to configuration changes without having to encapsulate every field
  * 
  * @author ryanm
  */
